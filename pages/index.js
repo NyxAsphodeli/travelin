7deb36069dbbb3215d72a1fcae91864d2a243a15
LauraMongi <laura.m@aworld.org>
2022-01-14 00:17:56 +0100

    home styling

--- pages/index.js
+++ pages/index.js
@@ -5,26 +5,79 @@ import Valori from "./valori"
 import Team from "./team"
 import Partners from "./partners"
 import Contatti from "./contatti"
-import placeholder from "../public/placeholder.jpg"
+import background from "../public/background.png"
+import menu from "../public/menu.png"
+import logoS from "../public/logo-s.png"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
       <Head>
         <title>Travelin</title>
         <meta name="description" content="Travelin official website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Image src={placeholder} alt="header image" />
-        <p>Breve presentazione simpatica non più di 8 righe della mission</p>
-        <Valori />
-        <Team />
-        <Partners />
+      <main>
+        <div
+          style={{
+            background: "#e4f9f6",
+            height: "15vh",
+            width: "100vw",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-around",
+          }}
+        >
+          <div
+            style={{ width: "50vw", display: "flex", justifyContent: "center" }}
+          >
+            <Image
+              className={styles.logoS}
+              src={logoS}
+              alt="logo travelin"
+              width="515px"
+              height="100px"
+              layout="intrinsic"
+              objectPosition="left"
+            />
+          </div>
+          <div
+            style={{ width: "30vw", display: "flex", justifyContent: "right" }}
+          >
+            <button
+              onClick={() => {
+                console.log("menù clicked")
+              }}
+            >
+              <Image src={menu} alt="menu" />
+            </button>
+          </div>
+        </div>
+        <div
+          style={{
+            background:
+              "#e4f9f6 linear-gradient(180deg, rgba(228,249,246,1) 59%, rgba(255,255,255,1) 100%)",
+          }}
+        >
+          <Image src={background} alt="header image" className={styles.logoS} />
+          <p className={styles.phrase}>Una nuova app sul turismo accessibile</p>
+        </div>
+        <div style={{ paddingInline: "5vw" }}>
+          <div style={{ height: "15vh" }} />
+          <Valori />
+          <div
+            className={styles.penguin}
+            style={{ height: "15vh", width: "100vw" }}
+          />
+          <Team />
+          <Partners />
+        </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer>
         <Contatti />
       </footer>
     </div>
--- pages/valori.js
+++ pages/valori.js
@@ -1,10 +1,33 @@
 function Valori() {
   return (
     <>
-      <h1>Valori</h1>
-      <p>
-        Breve presentazione simpatica non più di 8 righe,della mission della
-        start-up con riferimento ai pinguini
+      <h1 style={{ color: "#004267", fontSize: "2.2rem", fontWeight: "700" }}>
+        {"Valore all'Accessibilità"}
+      </h1>
+      <p
+        style={{
+          color: "#004267",
+          fontSize: "1.3rem",
+          justifyContent: "center",
+          textAlign: "center",
+          fontWeight: "500",
+        }}
+      >
+        La vita quotidiana è progettata per le persone comuni. Esistono però
+        individui extra-ordinari che ci permettono di andare oltre i limiti
+        della nostra realtà.
+      </p>
+      <p
+        style={{
+          color: "#004267",
+          fontSize: "1.3rem",
+          justifyContent: "center",
+          textAlign: "center",
+          fontWeight: "700",
+          fontStyle: "italic",
+        }}
+      >
+        Sei pronto/a ad abbattere i limiti?
       </p>
     </>
   )
