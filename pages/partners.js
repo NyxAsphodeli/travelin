cd5709a3acd0de6027ed4c77b44b0b778b317979
LauraMongi <laura.m@aworld.org>
2022-02-24 16:59:54 +0100

    partners

--- pages/partners.js
+++ pages/partners.js
@@ -1,4 +1,9 @@
 import styles from "../styles/Team.module.css"
+import Image from "next/image"
+import aprionlus from "../public/aprionlus.png"
+import culturaccess from "../public/culturaccess.png"
+import turismabile from "../public/turismabile.png"
+import federagit from "../public/federagit.jpg"
 
 function Partners() {
   return (
@@ -28,6 +33,18 @@ function Partners() {
           condividendo con noi {"l'intento"} di rendere <b>la bellezza</b>{" "}
           {"dell'Italia"} <b>a portata di tutti</b>{" "}
         </p>
+      </div>
+      <div style={{ marginBlockEnd: "50px" }}>
+        <Image src={aprionlus} alt="logo aprionlus" />
+      </div>
+      <div style={{ maxWidth: "60%", marginBlockEnd: "50px" }}>
+        <Image src={federagit} alt="logo federagit" />
+      </div>
+      <div style={{ maxWidth: "60%", marginBlockEnd: "50px" }}>
+        <Image src={culturaccess} alt="logo più cultura accessibile" />
+      </div>
+      <div style={{ marginBlockEnd: "50px" }}>
+        <Image src={turismabile} alt="logo turismabile" />
       </div>
     </div>
   )
